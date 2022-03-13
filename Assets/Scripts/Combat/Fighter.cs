a2c493fba63ec92603cc216b6a34c73e9e8f09e4
Lewis Williams <lewiswills393@gmail.com>
2022-03-13 17:33:27 +0000

    Added Health

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -10,6 +10,7 @@ namespace RPG.Combat
     {
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
 
         Transform target;
         float timeSinceLastAttack = 0;
@@ -33,10 +34,20 @@ namespace RPG.Combat
         {
             if(timeSinceLastAttack > timeBetweenAttacks)
             {
+                //this will trigger the Hit() event.
+
                 GetComponent<Animator>().SetTrigger("attack");
                 timeSinceLastAttack = 0;
+                
             }
             
+        }
+
+        //Animation Event
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange()
@@ -55,11 +66,7 @@ namespace RPG.Combat
             target = null;
         }
 
-        //Animation Event
-        void Hit()
-        {
-
-        }
+        
     }
 }
 
